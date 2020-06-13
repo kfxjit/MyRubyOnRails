0433b0705d6866cb905065e91d83bcaf2776689e
kfxjit <kfxjit@Owner-no-iMac-4.local>
2020-06-13 16:55:46 +0900

    [mod] make msgboard service   attention! not worked...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'msgboard', to: 'msgboard#index'
+  post 'msgboard', to: 'msgboard#index'
+  get 'msgboard/index'
+  post 'msgboard/index'
   get 'msgboard/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'hello/index'
