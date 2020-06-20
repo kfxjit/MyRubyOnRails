88504acef237b744711a41f9cef8c68ebc677d48
quiet396 <kohei.ingenious3.fx@hotmail.com>
2020-06-20 17:47:57 +0900

    [add] push the strange things

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'people/index'
+  get 'people', to: 'people#index'
   get 'people/add'
   post 'people/add', to: 'people#create'
   get 'people/:id', to: 'people#show'
