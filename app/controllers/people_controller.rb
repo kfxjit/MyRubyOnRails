74f079ae1444e6b3c5c808aac211aa3916a3b054
quiet396 <kohei.ingenious3.fx@hotmail.com>
2020-06-20 17:35:36 +0900

    [mod] create show action & template

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,13 @@
 class PeopleController < ApplicationController
+
   def index
     @msg = 'Person data.'
     @data = Person.all
   end
+
+  def show
+  @msg = "Indexed data."
+  @data = Person.find(params[:id])
+  end
+  
 end
