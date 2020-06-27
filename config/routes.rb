ad115cae288ac8ca8788d416d698f7ef79a6438c
quiet396 <kohei.ingenious3.fx@hotmail.com>
2020-06-27 16:01:18 +0900

    [add] create reading card application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+  get 'cards/show'
+  get 'cards/add'
+  post 'cards/add'
+  get 'cards/edit'
+  get 'cards/:id', to: 'cards#show'
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+  get 'cards/delete/:id', to: 'cards#delete'
+
   get 'people/index'
   get 'people', to: 'people#index'
   get 'people/add'
@@ -7,11 +18,13 @@ Rails.application.routes.draw do
   get 'people/edit/:id', to: 'people#edit'
   patch 'people/edit/:id', to: 'people#update'
   get 'people/delete/:id', to: 'people#delete'
+
   get 'msgboard', to: 'msgboard#index'
   post 'msgboard', to: 'msgboard#index'
   get 'msgboard/index'
   post 'msgboard/index'
   get 'msgboard/index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'hello/index'
   get 'hello/other'
