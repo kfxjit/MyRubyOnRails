a35b1981738443c8dcee7f0d557e4a30ca03ee3a
quiet396 <kohei.ingenious3.fx@hotmail.com>
2020-07-04 15:25:09 +0900

    [add] prepare find page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'people', to: 'people#index'
   get 'people/add'
   post 'people/add', to: 'people#create'
+  get 'people/find'
+  post 'people/find'
   get 'people/:id', to: 'people#show'
   get 'people/edit/:id', to: 'people#edit'
   patch 'people/edit/:id', to: 'people#update'
